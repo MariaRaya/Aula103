00048dde52b06e721f27abf3fdb2b4aa6b1fb697
MariaRaya <94574813+MariaRaya@users.noreply.github.com>
2023-09-26 09:38:35 -0300

    Update aula103.py

--- aula103.py
+++ aula103.py
@@ -1,28 +1,48 @@
+import sys
 import time
-import os 
-import shutil	
-from whatchdog.observers import Observer		
-from watchdog.events import FileSystemEventHandler 
+import random
 
-from_dir = "C:\Users\Maria Eduarda"
-to_dir = 
+import os
+import shutil
 
-## GERENCIA O EVENTO DE CRIAÇÃO DE UM NOVO ARQUIVO
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "DIGITE O CAMINHO DA PASTA DE DOWNLOAD (USE " / ") no VSC"
+# to_dir = "DIGITE A PASTA DE CAMINHO DE DESTINO (USE " / ") no VSC"
+
+from_dir = "C:/Users/preet/Downloads"
+to_dir = "C:/Users/preet/Desktop/Arquivos_baixados"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Classe Gerenciadora de Eventos
 
 class FileMovementHandler(FileSystemEventHandler):
 
-	def on_created(self, event):
-    
-    	print(event)
-      
-event_handler = FileMovementHandler() ## INICIALIZA A CLASSE GERENCIADORA DE EVENTOS
-    
+    def on_created(self, event):
+        print(event)
+        print(event.src_path)
+
+
+# Inicialize a Classe Gerenciadora de Eventos
+event_handler = FileMovementHandler()
+
+# Inicialize o Observer
 observer = Observer()
-    
+
+# Agende o Observer
 observer.schedule(event_handler, from_dir, recursive=True)
-    
+
+# Inicie o Observer
 observer.start()
 
+
 while True:
-	time.sleep(2)
+    time.sleep(2)
     print("executando...")
